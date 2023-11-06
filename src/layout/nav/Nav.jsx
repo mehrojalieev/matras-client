25fe799a6bb82f593a1ca3b6ebdcb67f8d779cc5
mehrojalieev <aliyevmehrojbek0@gmail.com>
2023-11-06 16:38:12 +0500

    Just Test

--- src/layout/nav/Nav.jsx
+++ src/layout/nav/Nav.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
-
+import "./Nav.scss"
+// import { Container } from "../../utils/Utils"
+import { NavLink, Link } from "react-router-dom"
+import Logo from "../../assets/images/logo.svg"
 const Nav = () => {
   return (
-    <div>Nav</div>
+
+      <nav>
+        <div className="nav__logo">
+          <img src={Logo} alt="Logo of Company" />
+          <h1>DREAM CLOUD</h1>
+        </div>
+        <div className="nav__menu-container">
+          <ul className="nav-menu">
+            <li>
+              <NavLink className={({ isActive }) => isActive ? " link link--active" : "link"} to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink className={({ isActive }) => isActive ? "link link--active" : "link"} to="katalog">Katalog</NavLink>
+            </li>
+            <li>
+              <NavLink className={({ isActive }) => isActive ? "link link--active" : "link"} to="about">Biz haqimizda</NavLink>
+            </li>
+            <li>
+              <NavLink className={({ isActive }) => isActive ? " link link--active" : "link"} to="address">Manzilimiz</NavLink>
+            </li>
+            <li>
+              <NavLink className={({ isActive }) => isActive ? " link link--active" : "link"} to="contact">Aloqa</NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
   )
 }
 
