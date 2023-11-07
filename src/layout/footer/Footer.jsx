0eb2965cc61b3dedb81d71fff6d0e799e3dea56f
mehrojalieev <aliyevmehrojbek0@gmail.com>
2023-11-07 22:08:58 +0500

    I did footer

--- src/layout/footer/Footer.jsx
+++ src/layout/footer/Footer.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
-
+import "./Footer.scss"
+import { Link } from "react-router-dom"
+import {AiFillFacebook, AiOutlineTwitter, AiOutlineInstagram, AiFillYoutube} from "react-icons/ai"
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+      <footer>
+        <div className="footer__menu">
+          <ul>
+            <li><Link>Biz haqimizda</Link></li>
+            <li><Link>Katalog</Link></li>
+            <li><Link>Aksiya</Link></li>
+            <li><Link>Manzilimiz</Link></li>
+          </ul>
+        <div className="footer__icons">
+        <a href="#"><AiFillFacebook/></a>
+        <a href="#"><AiOutlineTwitter/></a>
+        <a href="#"><AiOutlineInstagram/></a>
+        <a href="#"><AiFillYoutube/></a>
+        </div>
+        </div>
+
+      </footer>
+    </>
   )
 }
 
--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -2,12 +2,14 @@ import Nav from "../../layout/nav/Nav"
 import Hero from "../../components/hero/Hero"
 import Features from "../../components/features/Features"
 import Statics from "../../components/statics/Statics"
+import Footer from "../../layout/footer/Footer"
 const Home = () => {
   return (
     <>
       <Hero/>
       <Statics/>
       <Features/>
+      <Footer/>
     </>
 
   )
