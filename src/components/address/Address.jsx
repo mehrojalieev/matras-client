97b69ea46b8ddecd70363da68c81635900bc8524
mehrojalieev <aliyevmehrojbek0@gmail.com>
2023-11-08 16:54:59 +0500

    I connected the Api of Address, but it was made worse by new backend-developers

--- src/components/address/Address.jsx
+++ src/components/address/Address.jsx
@@ -1,22 +1,44 @@
 import "./address.scss"
 import { Container } from "../../utils/Utils"
-import {GoLocation} from 'react-icons/go'
+import { SlLocationPin } from 'react-icons/sl'
+import { GoLocation } from 'react-icons/go'
 
-import AddressImage  from "../../assets/images/address.png"
+import AddressImage from "../../assets/images/address.png"
+import { useEffect, useState } from "react"
+import { apiInstance } from "../../api"
+
 const Address = () => {
+
+  const [address, setAddress] = useState({})
+
+  useEffect(() => {
+    try {
+      apiInstance("/address")
+        .then(response => setAddress(response.data[0]))
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }, [])
+
+  console.log(address);
+
   return (
     <div className="address">
-        <Container>
-            <div className="address-wrapper">
-                <div className="address-content">
-                    <h3>Manzilimiz</h3>
-                    <h6>Toshkent, Parkent ko'chasi, 176-uy</h6>
-                    <p>Mo’ljal: Qoraqamish 2/1, Tursunxodjayeva ro’parasi, Milliy bog’ metro bekati.</p>
-                    <button><i><GoLocation/></i> Geolokatsiya</button>
-                </div>
-                <img className="address-image" src={AddressImage}  />
-            </div>
-        </Container>
+      <Container>
+        <div className="address-wrapper">
+          <div className="address-content">
+            <h3>
+              Manzilimiz
+              <i><SlLocationPin/></i>
+            </h3>
+            <h6> {address.location}</h6>
+            <p>Mo’ljal: {address.destination}</p>
+            <button><i><GoLocation /></i> Geolokatsiya</button>
+          </div>
+          <img className="address-image" src={AddressImage} />
+        </div>
+      </Container>
     </div>
   )
 }
