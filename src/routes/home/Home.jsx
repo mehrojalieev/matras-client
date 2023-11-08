b93a766ec0d32d0ca9feea4804e68d80e54f6cd5
mehrojalieev <aliyevmehrojbek0@gmail.com>
2023-11-08 14:31:25 +0500

    I made connect page and fixed some bugs

--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -3,12 +3,14 @@ import Hero from "../../components/hero/Hero"
 import Features from "../../components/features/Features"
 import Statics from "../../components/statics/Statics"
 import Footer from "../../layout/footer/Footer"
+import Address from "../../components/address/Address"
 const Home = () => {
   return (
     <>
       <Hero/>
       <Statics/>
       <Features/>
+      <Address/>
       <Footer/>
     </>
 
