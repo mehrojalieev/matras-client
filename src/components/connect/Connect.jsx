d966d180678ba83983942452515b72ea21058a52
Web-King007 <abbosxoliqberdiyev622@gmail.com>
2023-11-08 14:16:50 +0500

    Connect

--- src/components/connect/Connect.jsx
+++ src/components/connect/Connect.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import {Container} from "../../utils/Utils"
+import "./Connect.scss"
 
 const Connect = () => {
   return (
     <div className='connect-home'>
-        
+        <Container>
+            <div className="connect__wrapper">
+                <div className="connect-text">
+                    <h3 className='connect__title'>Sizni qiziqtirdimi?</h3>
+                    <p className='connect__word'>Raqamingizni qoldiring, biz sizga yana qo'ng'iroq qilamiz</p>
+                </div>
+           
+                <form className='connect-form'>
+                    <div className="from__wrapper">
+                        <span className="form__code">+998 |</span>
+                        <input type="text"  placeholder='Raqam yozing'/>
+                    </div>
+                    <button className='connect__submit-btn' type='submit'>Yuborish</button>
+                </form>
+            </div>
+        </Container>
     </div>
   )
 }
--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -3,12 +3,16 @@ import Hero from "../../components/hero/Hero"
 import Features from "../../components/features/Features"
 import Statics from "../../components/statics/Statics"
 import Footer from "../../layout/footer/Footer"
+import Connect from "../../components/connect/Connect"
+import Success from "../../components/connect/Success"
 const Home = () => {
   return (
     <>
       <Hero/>
       <Statics/>
       <Features/>
+      <Connect/>
+      {/* <Success/> */}
       <Footer/>
     </>
 
